778caa7d6b9dc4dd6df5f1c122e68277bc99d14a
Wojciech Nowak <wojciech.nowak@corp.smtsoftware.com>
2016-08-02 13:18:55 +0200

    Improve banner

--- CompanyWebsite/App_Start/BundleConfig.cs
+++ CompanyWebsite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace CompanyWebsite
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/customHomePage.css"));
         }
     }
 }
